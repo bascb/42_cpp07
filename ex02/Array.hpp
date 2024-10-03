e83d069a47c3a273d49555cbb7e4850f67039c57
Bernardo Castelo Branco <bcastelo@c2r1s12.42lisboa.com>
2024-10-03 21:42:41 +0100

    First files for ex02

---
+++ ex02/Array.hpp
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Array.hpp                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: bcastelo <bcastelo@student.42lisboa.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/10/03 21:34:58 by bcastelo          #+#    #+#             */
+/*   Updated: 2024/10/03 21:40:59 by bcastelo         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef ARRAY_HPP
+#define ARRAY_HPP
+
+#include <exception>
+
+template <typename T>
+class Array
+{
+    
+}
+
+#endif
